d470a79cea24803c24978e4f6c6825b7e0d11723
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2019-12-24 14:53:36 -0600

    Simple example with react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () => {
+  return <div>PageOne</div>
+}
+
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo
+      <button>Click me</button>
+    </div>
+  )
+}
+
 
 const App = () => {
   return(
     <div>
-      App
+      <BrowserRouter>
+      <div>
+        <Route path="/" exact component={PageOne} />
+        <Route path="/pagetwo" component={PageTwo} />
+      </div>
+      </BrowserRouter>
     </div>
   )
 };
