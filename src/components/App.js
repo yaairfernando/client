dafd3f8e6a7f9c6788873393ea7b9dce2513c274
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2019-12-27 18:00:47 -0600

    Add the Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-
-const PageOne = () => {
-  return (
-    <div>
-      PageOne
-      <Link to="/pagetwo">PageTwo</Link>
-    </div>
-  )
-}
-
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <Link to="/">PageOne</Link>
-      <button>Click me</button>
-    </div>
-  )
-}
-
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
   return(
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
