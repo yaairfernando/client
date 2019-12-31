6bd55986f9a09fbb68e521c696c2eca9d7e820be
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2019-12-30 19:40:02 -0600

    Refactor semantic classes to optimize the validation

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -15,8 +15,9 @@ class StreamCreate extends React.Component {
   }
 
   renderInput = ( { input, label, meta } ) => {
+    const className = `field ${meta.error && meta.touched ? 'error': ''}`
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
         <input {...input} autoComplete="off" />
         {this.renderError(meta)}
@@ -29,7 +30,7 @@ class StreamCreate extends React.Component {
   }
   render() {
     return (
-    <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+    <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
       <Field name="title" component={this.renderInput} label="Enter Title" />
       <Field name="description" component={this.renderInput} label="Enter Description" />
       <button className="ui button primary">Submit</button>
