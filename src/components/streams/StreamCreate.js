e1730d85a060f50549b7cfe1a1ff308b760277e5
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2019-12-30 18:58:42 -0600

    Using touched to display errors messages

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,11 +2,24 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-  renderInput( { input, label } ) {
+  renderError({error, touched}) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">
+            {error}
+          </div>
+        </div>
+      );  
+    }
+  }
+
+  renderInput = ( { input, label, meta } ) => {
     return (
       <div className="field">
         <label>{label}</label>
-        <input {...input} />
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
       </div>
     )
   }
@@ -25,6 +38,19 @@ class StreamCreate extends React.Component {
   };
 };
 
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = 'You must enter a title'
+  }
+
+  if (!formValues.description) {
+    errors.description = 'You must enter a description'
+  }
+  return errors;
+}
+
 export default reduxForm({
-  form: 'StreamCreate'
+  form: 'StreamCreate',
+  validate
 })(StreamCreate);
