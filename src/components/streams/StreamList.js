74c4974f263d2598250ed7996806eedef849074e
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2020-01-09 10:33:41 -0600

    Adding helpet method to display edit and delete buttons for streams related to the current user Id

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -6,10 +6,27 @@ class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
+
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">
+            Edit
+          </button>
+          <button className="ui button negative">
+            Delete
+          </button>
+        </div>
+      )
+    }
+  }
+
   renderList() {
     return this.props.streams.map((stream) => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera"></i>
           <div className="content">
             {stream.title}
@@ -35,7 +52,8 @@ class StreamList extends React.Component {
 
 const mapStateToProps = (state) => {
   return {
-    streams: Object.values(state.streams)
+    streams: Object.values(state.streams),
+    currentUserId : state.auth.userId
   }
 }
 
