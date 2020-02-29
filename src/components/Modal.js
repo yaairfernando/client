b3a0a855d49e9f2057cafaa65310777d77cd98a4
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2020-02-29 14:23:19 -0600

    Showing a modal and adding click handlers

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import history from '../history'
 
 const Modal = props => {
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">
-        jdlkjlfdglfdh ghfkjghkf  
+    <div onClick={() => history.push('/')} className="ui dimmer modals visible active">
+      <div onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
+        <div className="header">Delete Stream</div>
+        <div className="content">
+          Are you sure?
+        </div>
+        <div className="actions">
+          <button className="ui button">Cancel</button>
+          <button className="ui primary button">Delete</button>
+        </div>
       </div>  
     </div>,
     document.querySelector("#modal")
